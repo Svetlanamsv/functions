c506883d1edd222753b9c5dcc8cd5cb3125e38eb
Svetlanamsv <svetikmsv34@gmail.com>
2020-09-10 15:23:00 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def my_function(firstarg, secondarg, thirdarg ):
+    x = firstarg + secondarg
+    if x > thirdarg :
+        print ("Addition two first arguments greater then third argument")
+
+    else :
+        print ("The third argument is greater than addition two first arguments " )
+
+
+my_function(1,2,0)
+my_function(0,2,10)
+
+
+
